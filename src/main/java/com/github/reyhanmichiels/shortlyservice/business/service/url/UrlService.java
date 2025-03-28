de818060f53190060db13714025bfd4e5f723126
reyhanmichiels <michiels.reyhan@gmail.com>
2025-03-29 03:34:51 +0700

    add Url creation functionality with validation and exception handling

---
+++ src/main/java/com/github/reyhanmichiels/shortlyservice/business/service/url/UrlService.java
@@ -0,0 +1,10 @@
+package com.github.reyhanmichiels.shortlyservice.business.service.url;
+
+import com.github.reyhanmichiels.shortlyservice.business.dto.url.CreateUrlRequest;
+import com.github.reyhanmichiels.shortlyservice.business.dto.user.UserDTO;
+
+public interface UrlService {
+
+    void create(UserDTO authUser, CreateUrlRequest param);
+
+}
---
+++ src/main/java/com/github/reyhanmichiels/shortlyservice/business/service/url/UrlServiceImpl.java
@@ -0,0 +1,39 @@
+package com.github.reyhanmichiels.shortlyservice.business.service.url;
+
+import com.github.reyhanmichiels.shortlyservice.business.dto.url.CreateUrlRequest;
+import com.github.reyhanmichiels.shortlyservice.business.dto.user.UserDTO;
+import com.github.reyhanmichiels.shortlyservice.business.entity.Url;
+import com.github.reyhanmichiels.shortlyservice.business.repository.url.UrlRepository;
+import com.github.reyhanmichiels.shortlyservice.handler.exception.DuplicateResourceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UrlServiceImpl implements UrlService {
+
+    private final UrlRepository urlRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public void create(UserDTO authUser, CreateUrlRequest param) {
+        try {
+            this.urlRepository.save(Url.builder()
+                    .shortUrl(param.getShortUrl())
+                    .originalUrl(param.getOriginalUrl())
+                    .userId(authUser.getId())
+                    .password(param.getPassword() != null
+                            ? this.passwordEncoder.encode(param.getPassword())
+                            : null
+                    )
+                    .createdBy(authUser.getId().toString())
+                    .build()
+            );
+        } catch (DataIntegrityViolationException e) {
+            throw new DuplicateResourceException(e.getMessage());
+        }
+    }
+
+}
